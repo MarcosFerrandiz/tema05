a7ebcbc0c66cb57dc5f590bbf51ab54a71c317de
MarcosFerrandiz <marcos149ft@gmail.com>
2024-11-07 20:14:59 +0100

    Ejercicio 10 comentado

--- src/main/java/com/marcosferrandiz/tema05/Strings/Ejercicio11.java
+++ src/main/java/com/marcosferrandiz/tema05/Strings/Ejercicio11.java
@@ -1,7 +1,16 @@
 package com.marcosferrandiz.tema05.Strings;
 
+import com.marcosferrandiz.tema05.libreria.IO;
+
 public class Ejercicio11 {
     public static void main(String[] args) {
+        String nombre = IO.solicitarString("Ingrese su nombre",1,75);
+        String primApell = IO.solicitarString("Ingrese su primer apellido", 1, 75);
+        String secApell = IO.solicitarString("Ingrese su segundo apellido", 1,75);
+        String nomComp = nombre + " "+ primApell + " "+ secApell;
+
+    }
+    public static void puntoA(String frase){
 
     }
 }
