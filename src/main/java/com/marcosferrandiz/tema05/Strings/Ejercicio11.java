31fa78fa908eebaf0a3a4d7a82bd4eec2702052b
MarcosFerranidz <marcos149ft@gmail.com>
2024-11-09 15:49:36 +0100

    Ejercicio 11 comentado

--- src/main/java/com/marcosferrandiz/tema05/Strings/Ejercicio11.java
+++ src/main/java/com/marcosferrandiz/tema05/Strings/Ejercicio11.java
@@ -8,9 +8,98 @@ public class Ejercicio11 {
         String primApell = IO.solicitarString("Ingrese su primer apellido", 1, 75);
         String secApell = IO.solicitarString("Ingrese su segundo apellido", 1,75);
         String nomComp = nombre + " "+ primApell + " "+ secApell;
+        puntoA(nomComp);
+        System.out.println();
+        puntoB(nomComp);
+        System.out.println();
+        puntoC(nomComp);
+        System.out.println();
+        puntoD(nomComp);
+        System.out.println();
+        puntoE(nomComp);
+        System.out.println();
+        puntoF(nomComp);
+        System.out.println();
+        puntoG(nomComp);
 
     }
-    public static void puntoA(String frase){
 
+    /**
+     * Muestra la frase en mayusculas, minusculas, y su longitud
+     * @param frase Es la frase introducida por el usuario
+     */
+    public static void puntoA(String frase){
+        System.out.println("Punto A:");
+        System.out.println("Todo minusculas: "+ frase.toLowerCase());
+        System.out.println("Todo mayusculas: "+ frase.toUpperCase());
+        System.out.println("Longitud contando los espacios: " + frase.length());
+    }
+
+    /**
+     * Muestra los 5 primeros caracteres de la frase, y si la frase es menor a 5, la muestra toda
+     * @param frase Es la frase introducida por el usuario
+     */
+    public static void puntoB(String frase){
+        System.out.println("Punto B:");
+        if (frase.length() >= 5) {
+            System.out.println(frase.substring(0,5));
+        }else {
+            System.out.println(frase);
+        }
+    }
+
+    /**
+     * Muestra los 2 ultimos caracteres de la frase, siempre y cuando la frase sea mayor a 2 caracteres
+     * @param frase Es la frase introducida por el usuario
+     */
+    public static void puntoC(String frase){
+        System.out.println("Punto C:");
+        if (frase.length() >= 2) {
+            System.out.println(frase.substring(frase.length()-2));
+        }else {
+            System.out.println(frase);
+        }
+    }
+
+    /**
+     * Muestrq el numero de ocurrencias del ultimo caracter de la frase
+     * @param frase Es la frase introducida por el usuario
+     */
+    public static void puntoD(String frase){
+        System.out.println("Punto D:");
+        char letraFinal = frase.charAt(frase.length()-1);
+        String letraFinalQueEs = frase.replaceAll("[^"+ letraFinal +"]","");
+        int ocur = letraFinalQueEs.length();
+        System.out.println("La ultima letra es "+ letraFinal+ " con una ocurrencia de "+ ocur + " veces");
+    }
+
+    /**
+     * Pone en una misma cadena todas las ocurrencias de el primer caracter en mayusculas
+     * @param frase Es la frase introducida por el usuario
+     */
+    public static void puntoE(String frase){
+        System.out.println("Punto E:");
+        char primLetra = frase.charAt(0);
+        String primLetraSol = frase.replaceAll("[^"+ primLetra +"]","");
+        System.out.println(primLetraSol.toUpperCase());
+    }
+
+    /**
+     * Añade 3 * a la frase por delante y por detras
+     * @param frase Es la frase introducida por el usuario
+     */
+    public static void puntoF(String frase){
+        System.out.println("Punto F:");
+        System.out.println("***"+frase+"***");
+    }
+
+    /**
+     * Lee la frase introducida y la muestra del reves
+     * @param frase Es la frase introducida por el usuario
+     */
+    public static void puntoG(String frase){
+        StringBuilder sb = new StringBuilder();
+        System.out.println("Punto G:");
+        System.out.println(sb.append(frase).reverse());
     }
 }
