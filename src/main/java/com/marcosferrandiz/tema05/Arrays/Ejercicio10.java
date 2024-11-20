daf20cf4c31649646b077c63ad074f9d5a5f4016
MarcosFerrandiz <marcos149ft@gmail.com>
2024-11-20 18:33:38 +0100

    10 comentado

--- src/main/java/com/marcosferrandiz/tema05/Arrays/Ejercicio10.java
+++ src/main/java/com/marcosferrandiz/tema05/Arrays/Ejercicio10.java
@@ -1,10 +1,33 @@
 package com.marcosferrandiz.tema05.Arrays;
 
+import com.marcosferrandiz.tema05.libreria.IO;
+
+import java.util.Arrays;
+
 public class Ejercicio10 {
     public static void main(String[] args) {
         final int TAMANYO_ARRAY = 10;
-        int[] arrayP = new int[TAMANYO_ARRAY];
-        int[] arrayS = new int[arrayP.length];
+        double[] arrayP = new double[TAMANYO_ARRAY];
+        IO.solicitarDatosArray(arrayP);
+        IO.arrayToString(arrayP,2);
+        System.out.println(Arrays.toString(otroArray(arrayP)));
 
+    }
+
+    /**
+     * Metodo que devulve si el valor del array si es mayor a 10 devuelve el numero tal cual, y si es menor devuelve -1
+     * @param array Es el array introducido con sus valores
+     * @return Devuelve si es menor de 10 (-1) o mayor a 10, devuelve el numero tal cual
+     */
+    public static double[] otroArray(double[] array){
+        double[] array2 = new double[array.length];
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i] > 10){
+                array2[i] = array[i];
+            } else if (array[i] <= 10){
+                array2[i] = -1;
+            }
+        }
+        return array2;
     }
 }
