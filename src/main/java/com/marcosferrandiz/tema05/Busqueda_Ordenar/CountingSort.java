f8596d0bfc1cef05b598af2f5aedcaa495df8f5f
MarcosFerrandiz <marcos149ft@gmail.com>
2024-11-26 17:32:23 +0100

    cosas de german de la burbuja y el counting sort

---
+++ src/main/java/com/marcosferrandiz/tema05/Busqueda_Ordenar/CountingSort.java
@@ -0,0 +1,30 @@
+package com.marcosferrandiz.tema05.Busqueda_Ordenar;
+
+import com.marcosferrandiz.tema05.libreria.IO;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        IO.rellenarArrayAleatoriamente(array,1,10);
+        ordenaContando(array);
+
+    }
+    public static void ordenaContando(int[] array){
+        final int DISTANCIA_MAXIMA = 100000;
+        ArrayEstadisticas arrayEstadisticas = new ArrayEstadisticas(array);
+        int min = arrayEstadisticas.getMin();
+        int max = arrayEstadisticas.getMax();
+        if (max - min + 1 > DISTANCIA_MAXIMA){
+            System.err.println("Hay demasiada dispersión en los datos");
+            return;
+        }
+
+        int[] arrayConteo = new int[max+1];
+        for (int i = 0; i < array.length ; i++) {
+            arrayConteo[array[i]]++;
+        }
+        System.out.println("Conteo: "+Arrays.toString(arrayConteo));
+    }
+}
